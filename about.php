c36ca7f39b787aec21b10081499f1ecf7f15c0eb
♰ Dvrxkuh <ioane.dark@gmail.com>
2025-04-07 11:52:58 +0200

    portfolio-v1

---
+++ about.php
@@ -0,0 +1,37 @@
+<li>
+    Développement Web
+    HTML
+    CSS
+    JavaScript
+    PHP
+    Développement Objet et Applications
+    Java
+    Kotlin
+    Python
+    C#
+
+    Frameworks
+    Laravel
+    Electron
+
+    SGBD
+    MySQL
+    SQLite
+
+    IDE
+    Visual Studio
+    Visual Studio Code
+    Android Studio
+    IntelliJ
+    Eclipse for Java
+
+    Gestion incidents
+    GLPI gestion de tickets/support
+    GLPI gestion de parc informatique
+
+    Gestion de travail collaboratif
+    Github
+    Gitlab
+    Asana
+    Microsoft Teams
+</li>
